a7dad768481cac34da81809f05bb7fe87fe375a5
Łukasz Mrozek <morskk@gmail.com>
2017-07-21 20:35:03 +0200

    OO-ify code

--- src/TfsVersion/TfsVersion/TfsVersion.cs
+++ src/TfsVersion/TfsVersion/TfsVersion.cs
@@ -24,25 +24,79 @@ namespace TfsVersion
 
         public override bool Execute()
         {
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Authorization = 
-                new AuthenticationHeaderValue(
-                    "Basic",
-                    Convert.ToBase64String(
-                        Encoding.ASCII.GetBytes(
-                            $":{PersonalAccessToken}")));
-            httpClient.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            dynamic response = 
-                new JsonSerializer().Deserialize(
-                    new JsonTextReader(
-                        new StringReader(
-                            httpClient.GetStringAsync(
-                                    new Uri(
-                                        $"{BaseUrl}/_apis/tfvc/changesets?api-version=1.0&searchCriteria.itemPath={ItemPath}&searchCriteria.orderBy=id desc&$top=1"))
-                                .Result)));
-            TopChangesetId = (int)response.value[0].changesetId;
+            TopChangesetId =
+                new TopChangesetRequest(
+                        new TopChangesetUri(BaseUrl, ItemPath),
+                        PersonalAccessToken)
+                    .Response()
+                    .TopChangesetId();
             return true;
+        }
+
+        private sealed class TopChangesetUri
+        {
+            private readonly string _itemPath;
+            private readonly string _baseUrl;
+
+            public TopChangesetUri(string baseUrl, string itemPath)
+            {
+                _baseUrl = baseUrl;
+                _itemPath = itemPath;
+            }
+
+            public static implicit operator Uri(TopChangesetUri uri)
+            {
+                return new Uri(
+                    $"{uri._baseUrl}/_apis/tfvc/changesets?api-version=1.0&searchCriteria.itemPath={uri._itemPath}&searchCriteria.orderBy=id desc&$top=1");
+            }
+        }
+
+        private sealed class TopChangesetRequest
+        {
+            private readonly Uri _uri;
+            private readonly string _personalAccessToken;
+
+            public TopChangesetRequest(Uri uri, string personalAccessToken)
+            {
+                _uri = uri;
+                _personalAccessToken = personalAccessToken;
+            }
+
+            public TopChangesetResponse Response()
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue(
+                        "Basic",
+                        Convert.ToBase64String(
+                            Encoding.ASCII.GetBytes(
+                                $":{_personalAccessToken}")));
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                return 
+                    new TopChangesetResponse(
+                        httpClient.GetStringAsync(_uri).Result);
+            }
+        }
+
+        private sealed class TopChangesetResponse
+        {
+            private readonly string _response;
+
+            public TopChangesetResponse(string response)
+            {
+                _response = response;
+            }
+
+            public int TopChangesetId()
+            {
+                dynamic responseAsJson =
+                    new JsonSerializer().Deserialize(
+                        new JsonTextReader(
+                            new StringReader(
+                                _response)));
+                return responseAsJson.value[0].changesetId;
+            }
         }
     }
 }
